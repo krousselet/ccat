9809af22a6aa4807d378593ee161e8fedd2ff554
Kevin Rousselet <krous74@gmail.com>
2025-02-05 11:27:00 +0100

    update

--- src/components/SwiperComponent.vue
+++ src/components/SwiperComponent.vue
@@ -3,8 +3,9 @@
     :slides-per-view="7"
     :space-between="spaceBetween"
     :pagination="{
-      hideOnClick: true
+      hideOnClick: false,
     }"
+    mousewheel-enabled="true"
     :breakpoints="{
       320: {
         slidesPerView: 1,
@@ -52,43 +53,6 @@ export default {
     type: Array,
     required: true,
     validator: (value) => value.every((slide) => 'title' in slide && 'description' in slide && 'details' in slide),
-    default: () =>[
-                { title: "A.T.C", description: "Air Traffic Control", details: "Il s'agit de la structure au sol permettant de gérer toute une partie du ciel, qui est lui-même découpé en quartiers" },
-                { title: "A.P.U", description: "Auxiliary Power Unit", details: ""  },
-                { title: "B.E.A", description: "Bureau d'Enquêtes et d'Analyses", details: ""  },
-                { title: "C.A.T", description: "Commercial Air Transport", details: ""  },
-                { title: "C.R.N.A.", description: "Centre en-Route de la Navigation Aérienne (Contrôle les aéronefs IFR)", details: ""  },
-                { title: "C.C.S", description: "Centre de Coordination Sauvetage", details: ""  },
-                { title: "C.I.V", description: "Centre d'Information de Vol (Informe les aéronefs VFR)", details: ""  },
-                { title: "C.C.A", description: "Cabin Crew Attestation", details: ""  },
-                { title: "C.R.D", description: "Child Restraint Device", details: ""  },
-                { title: "C.R.S", description: "Child Restraint System", details: ""  },
-                { title: "C.T.A", description: "Certificat de Transporteur Aérien", details: ""  },
-                { title: "D.G.A.C", description: "Direction Générale de l'Aviation Civile", details: ""  },
-                { title: "E.A.S.A", description: "European Aviation Safety Agency ( AESA Agence Européenne de la Sécurité Aérienne)", details: ""  },
-                { title: "F.I.R", description: "Flight Information Région (RIV)", details: ""  },
-                { title: "G.T.A", description: "Gendarmerie des Transports Aériens", details: ""  },
-                { title: "G.P.U", description: "Ground Power Unit", details: ""  },
-                { title: "I.A.T.A.", description: "International Air Transport Association", details: ""  },
-                { title: "I.F.E", description: "In-flight Entertainment | Système de divertissement", details: ""  },
-                { title: "L.M.E", description: "Liste Minimale d'Equipements | Minimal Equipment List", details: ""  },
-                { title: "M.O.P.S.C", description: "Maximum Operational Passenger Seating Configuration", details: ""  },
-                { title: "O.2", description: "Oxygène", details: ""  },
-                { title: "O.A.C.I", description: "Organisation de l'Aviation Civile Internationale", details: ""  },
-                { title: "P.A.F", description: "Police Aux Frontières", details: ""  },
-                { title: "P.A.X", description: "Passager", details: ""  },
-                { title: "P.E.Q", description: "Equipage", details: ""  },
-                { title: "P.C.B", description: "Personnel Complémentaire de Bord", details: ""  },
-                { title: "P.N.C", description: "Personnel Navigant Commercial", details: ""  },
-                { title: "P.S", description: "Personnel au Sol", details: ""  },
-                { title: "P.S.U", description: "Unité de Service aux Passagers | Passenger Service Unit", details: ""  },
-                { title: "R.E.X", description: "Retour d'expérience", details: ""  },
-                { title: "R.I.V", description: "Région d'Information de Vol | Flight Information Region", details: ""  },
-                { title: "S.A.R", description: "Search and Rescue", details: ""  },
-                { title: "S.O.P / O.P.S", description: "Procédures Standarts de travail en équipage de l'exploitant", details: "" },
-                { title: "G.T.A.", description: "Gendarmerie des Transports Aériens", details: ""  },
-                { title: "T.U.C / U.T.C", description: "Temps Universel Coordonné | Universal Time Coordinated", details: ""  },
-            ],
         },
     },
 };
@@ -149,9 +113,6 @@ swiper-container::part(bullet-active) {
         }
     }
 }
-// .title-container {
-//     margin: 2vw 25px;
-// }
 
 
 

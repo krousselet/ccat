ea39d999eb1032a9beb30ee2f541e433e1bcacd8
Kevin Rousselet <krous74@gmail.com>
2025-02-11 15:33:14 +0100

    animation plane / sun

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,114 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="animation-container">
+    <div class="plane" :class="{ animatePlane: isAnimated }">
+      <img :src="planeImage" alt="logo d'un avion">
+    </div>
+    <div class="sun" :class="{ animateSun: isAnimated }">
+      <img :src="sunImage" alt="logo d'un soleil">
+    </div>
   </div>
 </template>
 
 <script>
+import planeMobile from '@/assets/images/mobile/plane-mobile.svg'
+import planeDesktop from '@/assets/images/desktop/plane-desktop.svg'
+import sunMobile from '@/assets/images/mobile/sun-mobile.svg'
+import sunDesktop from '@/assets/images/desktop/sun-desktop.svg'
+
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      isAnimated: false, // Controls when animation starts
+      windowWidth: window.innerWidth, // Stores current window width
+    };
   },
+  computed: {
+    planeImage() {
+      return this.windowWidth >= 992 ? planeDesktop : planeMobile;
+    },
+    sunImage() {
+      return this.windowWidth >= 992 ? sunDesktop : sunMobile;
+    }
+  },
+  mounted() {
+    this.startAnimation();
+    window.addEventListener("resize", this.updateWindowWidth);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.updateWindowWidth);
+  },
+  methods: {
+    startAnimation() {
+      setTimeout(() => {
+        this.isAnimated = true;
+      }, 500); // Small delay before animation starts
+    },
+    updateWindowWidth() {
+      this.windowWidth = window.innerWidth;
+    }
+  }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.animation-container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  height: 800px;
+  width: 90%;
+  margin: 0 auto;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.plane, .sun {
+  position: absolute;
+  z-index: 10;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+/* Plane Animation */
+.animatePlane {
+  animation: planeMove 4s ease-in-out forwards;
 }
 
-a {
-  color: #42b983;
+/* Sun Animation */
+.animateSun {
+  animation: sunMove 4s ease-in-out forwards;
+}
+
+@keyframes planeMove {
+  from {
+    left: -100%;
+  }
+  to {
+    left: 30%;
+  }
+}
+
+@keyframes sunMove {
+  from {
+    left: 100%;
+    bottom: -100px;
+  }
+  to {
+    left: 50%;
+    bottom: 70%;
+  }
+}
+
+@media (min-width: 320px) and (max-width: 991px) {
+  .animation-container {
+    max-width: 991px;
+    max-height: 400px;
+  }
+}
+
+@media (min-width: 992px) and (max-width: 2048px) {
+  .animation-container {
+    max-width: 2048px;
+    max-height: 800px;
+  }
 }
 </style>
